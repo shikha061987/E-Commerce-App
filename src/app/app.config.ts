e69c0269de530224c979cf712ccbe71a9d445218
Manish Rawat <rawatmani@gmail.com>
2025-04-22 09:51:44 -0400

    config file changed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
-/*
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};*/
-export const appConfig = {
-  providers: [
-    provideRouter(routes, withHashLocation()) // Enables hash-based routing
-  ]
+  providers: [ provideRouter(routes),provideHttpClient()]
 };
+
